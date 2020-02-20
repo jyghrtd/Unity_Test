4b411040db022f24eb9ebc4cfc1d5ef9a3882a6f
ekjkkk153 <ekjkkk153@daum.net>
2020-02-20 21:36:23 +0900

    민규가 몬헌 사준대

---
+++ Assets/Script/MenuEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuEvent : MonoBehaviour
+{
+
+    public static MenuEvent instance;
+    public GameObject SaveBtn;
+    public bool isMenuOpened = false;
+
+    public void OnClickMenuBtn()
+    {
+        isMenuOpened = !isMenuOpened;
+    }
+
+    
+
+    void Update()
+    {
+        if (isMenuOpened)
+        {
+            SaveBtn.gameObject.SetActive(true);
+        }
+        else
+        {
+            SaveBtn.gameObject.SetActive(false);
+        }
+    }
+
+    
+
+}
---
+++ Assets/Script/SaveEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveEvent : MonoBehaviour
+{
+    public void OnClickSaveBtn()
+    {
+        Debug.Log("Clicked Save Button");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
